56f4094ebae7ef39b1f975205635d1113e4b3f7d
Gissele <ustr031124@ugb.edu.sv>
2025-08-29 09:03:10 -0600

    formulario basico

---
+++ ventana02.py
@@ -0,0 +1,10 @@
+import _tkinter as tk
+
+class Aplicacion:
+    def __init__(self):
+        formulario01 = tk.TK()
+        formulario01.title('Gissele')
+        formulario01.mainloop
+
+
+llamada = Aplicacion
